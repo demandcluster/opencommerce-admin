4791e4f11235270046a80e5d666d13aac696c04d
tedraykov <tedraykov@gmail.com>
2022-02-17 19:43:12 +0200

    refactor: change admin preview port

--- apps/admin/vite.config.ts
+++ apps/admin/vite.config.ts
@@ -4,6 +4,9 @@ import createImportPlugin from "vite-plugin-import";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  preview: {
+    port: 3000
+  },
   plugins: [
     createImportPlugin(
       [
